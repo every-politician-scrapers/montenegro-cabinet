9a7515f76d4a23bc43ce8cc33f62120e918a27a8
Tony Bowden <tony@tmtm.com>
2022-02-08 12:29:50 +0200

    compare enwiki President of the Parliament

---
+++ sources/speaker/scraper.rb
@@ -0,0 +1,28 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Party'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[no name start end].freeze
+    end
+
+    def empty?
+      (tds[1].text == tds[2].text) || (startDate.split('-').first.to_i < 1992)
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
